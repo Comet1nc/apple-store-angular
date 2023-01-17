417769f64ceac5691a2c306dc9304d53f3ca9a13
Ivan <zrosji@gmail.com>
2023-01-17 23:04:16 +0100

    search bar with animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate, query, group, stagger } from '@angular/animations';
+import { trigger, style, transition, animate, query, group, stagger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,11 +6,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   animations: [
+    trigger('searchview', [
+      transition(':enter', [
+        style({
+          opacity: '1',
+          background: 'transparent'
+        }),
+        group([
+          animate(700, style({
+            background: '*'
+          })),
+          query('.search-bar', [
+            style({
+              opacity: '0'
+            }),
+            animate(400, style({
+                opacity: '0',
+                transform: 'translateX(20px)'
+              })
+            ),
+            animate('300ms ease-out', style({
+              opacity: '1',
+              transform: '*'
+            }))
+          ]),
+          query('.search-results', [
+            style({
+              opacity: '0'
+            }),
+            animate(100, style({
+              opacity: '0'
+            })),
+            group([
+              animate(150, style({
+                opacity: '1'
+              })),
+              query('li', [
+                style({
+                  opacity: '0',
+                  transform: 'translateX(20px)'
+                }),
+                stagger('50ms ease-out', [
+                  animate(150, style({
+                    opacity: '1',
+                    transform: '*'
+                  }))
+                ])
+              ])
+            ])
+          ])
+        ]),
+      ]),
+      transition(':leave', [
+        group([
+          style({
+            opacity: '1'
+          }),
+          animate(150, style({
+            opacity: '0'
+          }))
+        ]),
+      ])
+    ]),
     trigger('navbar', [
-      state('closedSearch', style({
-        opacity: '0',
-        transform: 'scale(1)'
-      })),
       transition(':enter', [
         group([
           query('li', [
@@ -18,7 +76,7 @@ import { Component, OnInit } from '@angular/core';
               opacity: '0'
             }),
             stagger(50, [
-              animate(300, style({
+              animate(200, style({
                 opacity: '1'
               }))
             ])
@@ -28,7 +86,7 @@ import { Component, OnInit } from '@angular/core';
               transform: 'scale(0.5)'
             }),
             stagger(50, [
-              animate(600, style({
+              animate(400, style({
                 transform: 'scale(1)'
               }))
             ])
@@ -48,19 +106,35 @@ import { Component, OnInit } from '@angular/core';
               transform: 'scale(0.5)'
             }))
           ]))
-        ])
-        
+        ])  
       ])
-      
     ])
   ]
 })
 export class HeaderComponent implements OnInit {
-  animActive: boolean = false
+  
   navBarIsVisible: boolean = true
+  searchViewIsVisible: boolean = false
+  
   constructor() { }
   
   ngOnInit(): void {
+  }
+
+  ToggleNavbarVisibility() {
+    this.navBarIsVisible = !this.navBarIsVisible
+  }
+
+  ShowSearchView() {
+    this.ToggleNavbarVisibility()
+    this.searchViewIsVisible = true
+  }
+
+  CloseSearchView() {
+    this.searchViewIsVisible = false
+    setTimeout(() => {
+      this.ToggleNavbarVisibility()
+    }, 150);
   }
 
 }
